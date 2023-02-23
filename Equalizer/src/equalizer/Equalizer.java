7f2b93e008b52d81fca1390889f5164a23eacd1c
Mancska <95695531+Mancska@users.noreply.github.com>
2023-02-23 16:33:58 +0100

    sorok

--- Equalizer/src/equalizer/Equalizer.java
+++ Equalizer/src/equalizer/Equalizer.java
@@ -4,16 +4,28 @@ package equalizer;
 import java.util.Random;
 
 public class Equalizer {
-
+    private static Random rnd =new Random();
     public static void main(String[] args) {
+        eq(3);
+        eq();
+        eq(5);
+        eq();
+        eq(7);
+    }
+    public static void eq() {
+        
+        int db= rnd.nextInt(3,8);
+        oszlop(db);
+    }
+
+    private static void oszlop(int db) {
+        String szin = "\u001B[45m";
+        for (int i = 0; i < db; i++) {
+            System.out.print(szin +" ");  
+        }
         System.out.println("");
     }
-    public static void eq(String[] args) {
-        Random rnd =new Random();
-        int db= rnd.nextInt(3,7);
-        for (int i = 0; i < db; i++) {
-            String osszeg= osszeg+"*";
-        }
+    public static void eq(int hossz) {
+        oszlop(hossz);
     }
-    
 }
