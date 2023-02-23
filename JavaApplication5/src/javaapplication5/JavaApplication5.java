0bc075d00d091b14d4c0a52f3d198f8c7521bfa0
Mancska <95695531+Mancska@users.noreply.github.com>
2023-02-23 15:17:40 +0100

    Add files via upload

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -2,20 +2,25 @@
 package javaapplication5;
 
 public class JavaApplication5 {
-
     public static void main(String[] args) {
+        elso10();
+        osszead(0, 0);
+        
+    }
+    public static int elso10() {
       int osszeg = 0;
         for (int i = 0; i < 10; i++) {
             osszeg += i;
             
         }
-        kiir("elsö 10 szám összege:"+osszeg);
+        return osszeg;
+        
     }
     public static void kiir(String szoveg) {
         System.out.println(szoveg);
     }
-    public static void osszead(int a,int b ) {
-        System.out.println("");
+    public static int osszead(int a,int b ) {
+       return a+b;
         
     }
 }
