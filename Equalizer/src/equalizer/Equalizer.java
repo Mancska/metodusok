2be30331b3a373856e85d2c10fe282b50f95d140
Mancska <95695531+Mancska@users.noreply.github.com>
2023-02-23 16:22:11 +0100

    uj

--- Equalizer/src/equalizer/Equalizer.java
+++ Equalizer/src/equalizer/Equalizer.java
@@ -1,10 +1,19 @@
 
 package equalizer;
 
+import java.util.Random;
+
 public class Equalizer {
 
     public static void main(String[] args) {
-       
+        System.out.println("");
+    }
+    public static void eq(String[] args) {
+        Random rnd =new Random();
+        int db= rnd.nextInt(3,7);
+        for (int i = 0; i < db; i++) {
+            String osszeg= osszeg+"*";
+        }
     }
     
 }
