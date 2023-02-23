b43c04c074b8ea1dcebcb8fb5f2ab59778d3eda4
Mancska <95695531+Mancska@users.noreply.github.com>
2023-02-23 15:51:27 +0100

    commit

--- oraimunka/src/javaapplication5/JavaApplication5.java
+++ oraimunka/src/javaapplication5/JavaApplication5.java
@@ -10,7 +10,11 @@ public class JavaApplication5 {
        int szam1 = 7, szam2=4;
        osszeg = osszead(szam1,szam2);
        kiir("%d + %d = %d\n.".formatted(szam1,szam2,osszeg));
-        
+        osszeg =osszead(1, 2);
+        osszeg =osszead(osszeg, 3);
+        osszeg =osszead(osszeg, 4);
+        kiir("1+2+3+4="+osszeg+"\n");
+        osszeg=3+4+2;
     }
     public static int elso10() {
       int osszeg = 0;
