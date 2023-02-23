b8b00baf3f927f19e0b4ac730a617e21b0dc96a2
Mancska <95695531+Mancska@users.noreply.github.com>
2023-02-23 15:33:54 +0100

    uj

---
+++ oraimunka/src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,29 @@
+
+package javaapplication5;
+
+public class JavaApplication5 {
+    public static void main(String[] args) {
+       int osszeg= elso10();
+       String kimenet = "az elso 10 szám osszege: "+osszeg+ "\n";
+       kiir(kimenet);
+       
+       int szam1 = 7, szam2=4;
+       osszeg = osszead(szam1,szam2);
+       kiir("%d + %d = %d\n.".formatted(szam1,szam2,osszeg));
+        
+    }
+    public static int elso10() {
+      int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg += i;
+        }
+        return osszeg;
+    }
+    public static void kiir(String szoveg) {
+        System.out.println(szoveg);
+    }
+    public static int osszead(int a,int b ) {
+       return a+b;
+        
+    }
+}
